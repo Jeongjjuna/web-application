4a787cd7a9c68845f5eb88bfe82b9d8c8322aee5
jeongjihun <wlgns2234@naver.com>
2024-11-18 01:26:22 +0900

    init: mvc 프레임워크 만들기

---
+++ mvc/src/main/java/WebApplicationServer.java
@@ -0,0 +1,6 @@
+public class WebApplicationServer {
+    
+    public static void main(String[] args) {
+        System.out.println("mvc 프레임워크 만들기");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "web-application"
 include("tomcat")
+include("mvc")
